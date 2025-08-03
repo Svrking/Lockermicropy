f4fab81aee1bdfa577e676202d5d3c4fe7defd3c
Svrking <svrixx234@gmail.com>
2025-08-03 17:18:54 -0500

    LCD Screen

--- Lockerpy/Master/main.py
+++ Lockerpy/Master/main.py
@@ -1,37 +1,102 @@
 import network
 import espnow 
 import machine as mc
+from DIYables_MicroPython_LCD_I2C import LCD_I2C
 import time
 
-#variables
-ulcmnd = None
+pressedbutton = 0 
+ulcmnd = None 
+maximummodes = 2
+lastpressedbutton = None
+lcdstate = None
+llastpressedbutton = None
+Open = "RlQmXpne02MC6}Fdnt#8=SD?G,Xi"
+Close = ";E7_+xtQpj83+!@x~1Nh>[4vzu65"
+key = b"pSW51#zEa7Tk6XrQ"
+I2C_ADDR = 0x27 
+LCD_ROWS = 2 
+LCD_COLS = 16
+ledopa = mc.Pin(14, mc.Pin.OUT)
+ledcla = mc.Pin(12, mc.Pin.OUT)
 button = mc.Pin(4, mc.Pin.IN)
+button1 = mc.Pin(16, mc.Pin.IN)
+#Signals of activation:
 led = mc.Pin(2, mc.Pin.OUT)
-
+i2c = mc.I2C(1, scl=mc.Pin(22), sda=mc.Pin(21), freq=400000)
+lcd = LCD_I2C(i2c, I2C_ADDR, LCD_ROWS, LCD_COLS)
+lcd.backlight_on()
+lcd.clear()
+#activating connection ESPNOW
 sta = network.WLAN(network.WLAN.IF_STA)
-sta.active(True) # A WLAN interface must be active to send()/recv()
-sta.disconnect() # Because ESP8266 auto-connects to last Access Point
+sta.active(True) 
+sta.disconnect() 
 led.value(False)
-
 e = espnow.ESPNow()
 e.active(True)
-
-peer = b"\x88\x13\xbf\x82\x37\x88" # identifier of the other ESP, the one esp we will connect
-e.add_peer(peer)      # Must add_peer() before send()
-
-token ="LIEsw6qMzx99OHWKOmdhQXVMxS213Xt8R4cJnw3RYRmQyVqvo2" 
-
-while True:
- 
+peer = b"\x88\x13\xbf\x82\x37\x88" 
+e.add_peer(peer, lmk = key) 
+lcd.clear()
+#tokens for different modes:
+tokenmodebed = "LIEsw6qMzx99OHWKOmdhQXVMxS213Xt8R4cJnw3RYRmQyVqvo2" 
+tokenmodelab = "J6pGUJscy1ZOdKuuFbXrRJtWBV916im8MhRhf3GY5TDmtFuWkfBPg6Gy" 
+tokens = (tokenmodebed, tokenmodelab) #tokenlab, tokenbed
+def loadc():
+    try:
+        with open("counter.txt") as f:
+            return int(f.read())  # o int()
+    except:
+        return 2  # valor inicial
+def savec(c):
+ try:
+  with open("counter.txt", "w") as f:
+   f.write(str(c))
+ except Exception as e:
+  print("Error guardando contador:", e)
+def activation(): # def for the activation
  if button.value() == True: 
-    mensaje = "Open"
- 
+  return Open #Open
  else:
-    mensaje = "Close"
- 
- if mensaje != ulcmnd:
-   msg = f"{mensaje}:{token}".encode()
-   e.send(peer, msg)
+  return Close #Closed
+class LockerOutputs: #sends tokens
+ def __init__(self,token): 
+  self.token = token
+class LCDMessage: #makes shorter the code since I do not have to write those 3 lines
+ def __init__(self,lcdmsg):
+  self.lcdmsg = lcdmsg
+  lcd.clear()
+  lcd.set_cursor(0,0)
+  lcd.print(self.lcdmsg)
+c = loadc()
+while True:
+ mensaje = activation() # the def is equal to mensaje
+ if button1.value(): 
+  pressedbutton += 1
+  time.sleep(0.5)
+ if pressedbutton == 0:
+  token1 = LockerOutputs(tokens[0]) 
+ elif pressedbutton == 1:
+  token1 = LockerOutputs(tokens[1])
+ if pressedbutton > maximummodes - 1: 
+  pressedbutton = 0
+  print("Reboot")
+ try:  
+  if mensaje != ulcmnd: # Sends 1 message to the receptor:
+   msg = f"{mensaje}:{token1.token}:{c}".encode()
+   print(token1.token)
+   e.send(peer, msg) # Sends the message:
+   print(f"Veces {c}")
    print(f"Enviado: {mensaje}")
- 
+   if mensaje == Open:
+    LCDMessage("Opened")
+    ledopa.value(True)
+    ledcla.value(False)
+   else:
+    LCDMessage("Closed")
+    ledopa.value(False)
+    ledcla.value(True)
+   c = int(c + 3)
+   savec(c)
+ except:
+  print("Trying again")
+  continue
  ulcmnd = mensaje
--- Lockerpy/Slave/main.py
+++ Lockerpy/Slave/main.py
@@ -3,31 +3,40 @@ import espnow
 import machine as mc
 import time
 
-#ESPNOW conection
-
 sta = network.WLAN(network.WLAN.IF_STA)
 sta.active(True) # A WLAN interface must be active to send()/recv()
 sta.disconnect() # Because ESP8266 auto-connects to last Access Point
 e = espnow.ESPNow()
 e.active(True)
-
 #variables
-
 rele = mc.Pin(4, mc.Pin.OUT)
 led = mc.Pin(2, mc.Pin.OUT) 
-
 #security
-
 tkn = "LIEsw6qMzx99OHWKOmdhQXVMxS213Xt8R4cJnw3RYRmQyVqvo2" #token
 mac = [b"\x88\x13\xbf\x82*\xe0"] #88:13:bf:82:2a:e0 normal mac
-
+key = b"pSW51#zEa7Tk6XrQ"
+e.add_peer(mac[0], lmk=key)
 #LED
-
 def lightwait():
    led.value(True)
    time.sleep(0.5)
    led.value(False)
    time.sleep(0.5)
+def loadc():
+    try:
+        with open("counter.txt") as f:
+            return int(f.read())  # o int()
+    except:
+        return 2  # valor inicial
+def savec(c):
+ try:
+  with open("counter.txt", "w") as f:
+   f.write(str(c))
+ except Exception as e:
+  print("Error guardando contador:", e)
+
+c = loadc()
+last_c = c 
 
 while True:
  host, msg = e.recv() # msg == None if timeout in recv()
@@ -40,22 +49,31 @@ while True:
    continue
    
   try:
-        cmd, token = msg.decode().split(":") # split the code in three parts: (host, msg) msg = cmd and tkn  
-        print("Comando:", cmd)
-        print("Token recibido")
-        
-        if token != tkn:
-         print("Not today, bud!")
-         continue
-        
-        else: 
-         if cmd == "Open":
-           rele.value(True)
-           led.value(True)
-         elif cmd == "Close":
-           rele.value(False)
-           led.value(False)         
+   cmd, token, c = msg.decode().split(":") # split the code in three parts: (host, msg) msg = cmd and tkn  
+   c = int (c)
+   print("Comando:", cmd)
+   print("Token recibido")
+
+   if token != tkn:
+     print("Not today, bud!")
+     continue
+   if c <= last_c:
+    print("Error")
+    continue
+    
+   last_c = c
+   
+   savec(last_c)
+
+   if cmd == "RlQmXpne02MC6}Fdnt#8=SD?G,Xi":
+    rele.value(True)
+    led.value(True)
+    print(f"Veces {last_c}")
+
+   elif cmd == ";E7_+xtQpj83+!@x~1Nh>[4vzu65":
+    rele.value(False)
+    led.value(False)         
        
   except ValueError:
-        print("Incorrect")
-        continue
+   print("Incorrect")
+   continue
